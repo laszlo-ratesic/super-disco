35f6543aaff5a8028ff460e9ae570abd7d1f2299
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-01-22 12:27:21 -0600

    implement moment functionality

--- script.js
+++ script.js
@@ -4,4 +4,44 @@ $('#currentDay').append(moment().format('dddd, MMMM Do'));
 // Declare css classes of 12 timeblocks based on relative time
 // There will always be 12 blocks***
 
-$('#hour-el').append(moment().format('h'));
+// Store current hour in a variable
+const hour = moment().format('H')
+
+console.log(hour);
+
+for (let i = 0; i < 13; i++) {
+    // creates a container row
+    const newRow = $('<div class="row time-block"></div>').attr('id', i);
+    const newHour = $('<div class="hour col-2 col-lg-1"></div>').attr('id', (i + 9));
+    if ((i + 9) < 12) {
+        newHour.text((i + 9) + 'am');
+    }
+    else if ((i + 9) === 12) {
+        newHour.text((i + 9) + 'pm');
+    }
+    else {
+        newHour.text((i - 3) + 'pm');
+    }
+    for(let j = 0; j < 3; j++) {
+        if (j < 1) {
+            $(newRow).append(newHour);
+        }
+        else if (j === 1) {
+            const textArea = $('<textarea class="col-8 col-lg-10"></textarea>');
+            if (newHour.attr('id') < hour || i === 0) {
+                textArea.addClass('past');
+            }
+            else if (newHour.attr('id') === hour) {
+                textArea.addClass('present');
+            }
+            else {
+                textArea.addClass('future');
+            }
+            $(newRow).append(textArea);
+        }
+        else {
+            $(newRow).append('<button class="saveBtn col-2 col-lg-1"></button>');
+        }
+    }
+    $('.container').append(newRow);
+}
